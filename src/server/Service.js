6ade138bb0986db34c3cd08921f81546e274430a
Lucas.Rodrigues <l.cunha14.lc@gmail.com>
2024-05-20 18:37:22 -0300

    chore: Update npm dependencies, add nodemailer package, and update form submission URL

--- src/server/Service.js
+++ src/server/Service.js
@@ -1,9 +1,14 @@
 // Service.js
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const url = process.env.URL_MAIN;
 
 export async function submitForm(data) {
     try {
-        const response = await axios.post('http://localhost:3000/form', data);
+        const response = await axios.post(`${url}/form`, data);
         return response.data;
     } catch (error) {
         console.error(error);
