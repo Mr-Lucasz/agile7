3bfada155e6a382f7cd773ddee5c9152dcaea8be
Lucas.Rodrigues <l.cunha14.lc@gmail.com>
2024-05-20 15:45:56 -0300

    chore: Update LottieButton component to handle form submission and display missing fields in a Snackbar

--- src/components/FormCTA.jsx
+++ src/components/FormCTA.jsx
@@ -1,32 +1,49 @@
+// FormCTA.jsx
 import { LottieButton } from "./LottieButton";
 import styles from "./FormCTA.module.css";
 import TextField from "@mui/material/TextField";
 import { useState } from "react";
-// checkbox mui material
 import Checkbox from "@mui/material/Checkbox";
-// snackbar mui material
 import Snackbar from "@mui/material/Snackbar";
 import Alert from "@mui/material/Alert";
-import { submitForm } from '../server/Service';
+import { submitForm } from "../server/Service";
 import { useForm } from "react-hook-form";
 
 export function FormCTA() {
-  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+    getValues,
+  } = useForm();
   const [open, setOpen] = useState(false);
 
+  // Add state for checkbox
+  const [checked, setChecked] = useState(false);
+
   const onSubmit = async (data) => {
+    // Check if data is defined and is an object
+    if (!data || typeof data !== "object") {
+
+      return;
+    }
+
+    // Check if all fields are filled and checkbox is checked
+    if (Object.values(data).includes("") || !checked) {
+      setOpen(true);
+      console.log("Please fill all fields and check the checkbox");
+      return;
+    }
     await submitForm(data);
-    setOpen(true);
     reset();
   };
-
   const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
     setOpen(false);
   };
-
 
   const textFieldStyles = {
     "& label.Mui-focused": {
@@ -55,83 +72,91 @@ export function FormCTA() {
     },
   };
 
-
   return (
-      <>
-        <form className={styles.formCta} onSubmit={handleSubmit(onSubmit)}>
-          <h2>Entre em contato com a AGILE7 TECH.</h2>
-          <p>Deixe suas informações e retornaremos o mais breve possível.</p>
+    <>
+      <form className={styles.formCta} onSubmit={handleSubmit(onSubmit)}>
+        <h2>Entre em contato com a AGILE7 TECH.</h2>
+        <p>Deixe suas informações e retornaremos o mais breve possível.</p>
 
-          <div className={styles.container1}>
-            <TextField
-                {...register("name", { required: true })}
-                sx={textFieldStyles}
-                id="outlined-basic"
-                label="Nome"
-                variant="outlined"
-                required={true}
-                fullWidth
-            />
-            <TextField
+        <div className={styles.container1}>
+          <TextField
+            {...register("nome", { required: true })}
+            sx={textFieldStyles}
+            id="outlined-basic"
+            label="Nome"
+            variant="outlined"
+            required={true}
+            fullWidth
+          />
+          <TextField
+            {...register("email", { required: true })}
+            sx={textFieldStyles}
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            required={true}
+            fullWidth
+          />
+        </div>
+        <div className={styles.container2}>
+          <TextField
+            {...register("telefone", { required: true })}
+            sx={textFieldStyles}
+            id="outlined-basic"
+            label="Telefone"
+            variant="outlined"
+            required={true}
+            fullWidth
+          />
+          <TextField
+            {...register("empresa", { required: true })}
+            sx={textFieldStyles}
+            id="outlined-basic"
+            label="Empresa"
+            variant="outlined"
+            required={true}
+            fullWidth
+          />
+        </div>
+        <div className={styles.container3}>
+          <TextField
+            {...register("mensagem", { required: true })}
+            sx={textFieldStyles}
+            id="outlined-multiline-static"
+            label="Nos fale mais sobre o seu negócio."
+            multiline
+            rows={4}
+            variant="outlined"
+            fullWidth
+          />
+        </div>
+        <div className={styles.container4}>
+          <Checkbox
+            {...register("checkbox", { required: true })}
+            checked={checked}
+            onChange={(e) => setChecked(e.target.checked)}
+            sx={{
+              color: "white",
+              "&.Mui-checked": {
+                color: "white",
+              },
+            }}
+          />
+          <p>
+            Eu concordo compartilhar esses dados para contato com a AGILE7 TECH.
+          </p>
+        </div>
 
-                sx={textFieldStyles}
-                id="outlined-basic"
-                label="Email"
-                variant="outlined"
-                required={true}
-                fullWidth
-            />
-          </div>
-          <div className={styles.container2}>
-            <TextField
-                sx={textFieldStyles}
-                id="outlined-basic"
-                label="Telefone"
-                variant="outlined"
-                required={true}
-                fullWidth
-            />
-            <TextField
-                sx={textFieldStyles}
-                id="outlined-basic"
-                label="Empresa"
-                variant="outlined"
-                required={true}
-                fullWidth
-            />
-          </div>
-          <div className={styles.container3}>
-            <TextField
-                sx={textFieldStyles}
-                id="outlined-multiline-static"
-                label="Nos fale mais sobre o seu negócio."
-                multiline
-                rows={4}
-                variant="outlined"
-                fullWidth
-            />
-          </div>
-          <div className={styles.container4}>
-            <Checkbox
-                sx={{
-                  color: "white",
-                  "&.Mui-checked": {
-                    color: "white",
-                  },
-                }}
-            />
-            <p>
-              Eu concordo compartilhar esses dados para contato com a AGILE7 TECH.
-            </p>
-          </div>
-
-          <LottieButton/>
-          <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-            <Alert onClose={handleClose} severity="success">
-              Form submitted successfully!
-            </Alert>
-          </Snackbar>
-        </form>
-      </>
+        <LottieButton
+            onSubmit={onSubmit}
+            formState={{ ...getValues(), checkbox: checked, ...errors }}
+        />
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="warning">
+            Por favor, preencha todos os campos e marque a caixa de seleção!
+          </Alert>
+        </Snackbar>
+      </form>
+    </>
   );
 }
--- src/components/LottieButton.jsx
+++ src/components/LottieButton.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react';
-import Lottie from 'react-lottie';
-import animationData from '../assets/Button.json'; // Caminho para o seu arquivo JSON
-import { submitForm } from '../server/Service';
+import { useState } from "react";
+import Lottie from "react-lottie";
+import animationData from "../assets/Button.json"; // Caminho para o seu arquivo JSON
+import PropTypes from "prop-types";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 
-export function LottieButton() {
+export function LottieButton({ onSubmit, formState }) {
   const [isHovered, setIsHovered] = useState(false);
+  const [missingFields, setMissingFields] = useState([]);
+  const [open, setOpen] = useState(false);
 
   const defaultOptions = {
     loop: true,
     autoplay: false, // Desligue o autoplay para controlar a animação com hover
     animationData: animationData,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid meet',
-    }
+      preserveAspectRatio: "xMidYMid meet",
+    },
   };
 
   // Funções para lidar com eventos de mouse
@@ -24,23 +28,62 @@ export function LottieButton() {
     setIsHovered(false);
   };
   const handleClick = async () => {
-    const data = {}; // Substitua isso pelos dados do formulário
-    await submitForm(data);
+    // Separate checkbox from other fields
+    const { checkbox, ...otherFields } = formState;
+
+    // Check if all fields are filled
+    const emptyFields = Object.entries(otherFields)
+      .filter(([key, val]) => val === "")
+      .map(([key]) => key);
+
+    // Check if checkbox is checked
+    if (!checkbox) {
+      emptyFields.push("checkbox");
+    }
+
+    console.log("Empty fields:", emptyFields); // Print out the empty fields
+
+    if (emptyFields.length > 0) {
+      setMissingFields(emptyFields);
+      setOpen(true);
+      return;
+    }
+
+    console.log("submit");
+    console.log("Form data:", formState); // Print out the form data
+
+    await onSubmit(formState); // Pass formState as argument to onSubmit
   };
 
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
 
   return (
-      <div
-          onClick={handleClick}
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-          style={{ width: '80%', height: 'auto', cursor: 'pointer'}}
-      >
-        <Lottie
-            options={defaultOptions}
-            isStopped={!isHovered} // A animação é interrompida se não estiver em hover
-            isPaused={!isHovered} // A animação é pausada se não estiver em hover
-        />
-      </div>
+    <div
+      onClick={handleClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      style={{ width: "80%", height: "auto", cursor: "pointer" }}
+    >
+      <Lottie
+        options={defaultOptions}
+        isStopped={!isHovered} // A animação é interrompida se não estiver em hover
+        isPaused={!isHovered} // A animação é pausada se não estiver em hover
+      />
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="warning">
+          Por favor, preencha os seguintes campos: {missingFields.join(", ")}
+        </Alert>
+      </Snackbar>
+    </div>
   );
 }
+
+LottieButton.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  formState: PropTypes.object.isRequired,
+};
