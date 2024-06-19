bad93accaf6cc24fc1cf74d7cae0796712753b59
Lucas.Rodrigues <l.cunha14.lc@gmail.com>
2024-06-19 01:04:24 -0300

    chore: Update package.json with Jest and related dependencies

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,23 +3,32 @@ module.exports = {
   env: {
     browser: true,
     es2020: true,
-    node: true  // Add this line
+    node: true,
+    'jest/globals': true 
   },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:jest/recommended' 
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: '18.2'
+    }
+  },
+  plugins: ['react-refresh', 'jest'], 
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
-  },
+      { allowConstantExport: true }
+    ]
+  }
 }
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-    moduleFileExtensions: ['js', 'jsx'],
-    setupFilesAfterEnv: ['./jest.setup.js'],
-  };
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['js', 'jsx'],
+  setupFilesAfterEnv: ['./jest.setup.js'],
+};
