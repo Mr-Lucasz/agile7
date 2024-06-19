b8e3ff91e2901d755cad91bceb259f10e58cf2fb
Lucas.Rodrigues <l.cunha14.lc@gmail.com>
2024-06-19 02:09:44 -0300

    chore: Update babel configuration with babel-plugin-transform-vite-meta-env

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,9 +1,18 @@
 // jest.config.mjs
 export default {
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy',
+    '\\.css$': 'identity-obj-proxy',
+  },
   transform: {
     '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.css$': 'jest-css-modules-transform',
   },
   moduleFileExtensions: ['js', 'jsx'],
   setupFilesAfterEnv: ['./jest.setup.js'],
+  setupFiles: ['jest-canvas-mock'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@mui|other-module)/)',
+  ],
 };
--- src/tests/FormCTA.test.js
+++ src/tests/FormCTA.test.js
@@ -1,4 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
+import React from 'react';
 import FormCTA from '../components/FormCTA';
 
 describe('FormCTA Component', () => {
